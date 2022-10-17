4de6f4ea480da785b0be728f8dd9f58baeb78fd6
Urwa Umer <urwa.umer@confiz.com>
2022-10-17 15:53:09 +0500

    created the env variable for aut key

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'cypress'
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
-});
+    env: {
+        'SIGN-UP-FOR-KEY': '',
+    },
+    video: false,
+})
