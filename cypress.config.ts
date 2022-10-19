5eb8189c4ce6a46f244ff001114a763c76e86b57
Urwa Umer <urwa.umer@confiz.com>
2022-10-19 13:58:30 +0500

    cypress-mochawesome-reporter added

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'cypress'
 
 module.exports = defineConfig({
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+        charts: true,
+        reportPageTitle: 'flooss-api-test-report',
+        embeddedScreenshots: true,
+        inlineAssets: true,
+        saveAllAttempts: false,
+        code: false,
+    },
     e2e: {
         setupNodeEvents(on, config) {
             // implement node event listeners here
+            require('cypress-mochawesome-reporter/plugin')(on)
         },
     },
     env: {
